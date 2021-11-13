0fd26fbcc9fbd619ddaf2850deddc9fa771a5430
taka70gi <taka.70.gi@gmail.com>
2021-11-13 21:08:54 +0900

    何度もすみません。修正版です。

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,7 +8,7 @@ class Task < ApplicationRecord
 
     def date_before_start
         return if end_on.blank? || start_on.blank?
-        errors.add(:end_on, "は開始日以降のものを選択してください") if end_on < start_on
+        errors.add(:start_on, "は今日以降のものを選択してください") if start_on.to_date < Date.today
     end
 
     def date_before_finish
